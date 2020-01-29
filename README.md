d8b384de770a209e54ee92f80b84a016547b72de
Luiz Rodrigo de Souza <me@lurodrigo.com>
2020-01-28 21:26:42 -0300

    Added everything

--- README.md
+++ README.md
@@ -1,22 +1,120 @@
-# firestore-client
+# firestore-clj
 
-A Clojure library designed to ... well, that part is up to you.
+A Firestore API for Clojure. Provides tools for doing both single pulls and streaming real-time data.
+This library is a thin wrapper over `com.google.firebase/firebase-admin`.
 
-## Usage
+## Installation and Docs
 
-FIXME
+Add to your `project.clj` dependencies:
+
+```[polvo/firestore-clj "0.1.0"]```
+
+You can read the docs on [clj-doc]().
+
+## Getting started
+You can use `client-with-creds` to get a client using credentials from a service account.
+
+```clojure
+(require '[firestore-clj.core :as db])
+
+(def db (db/client-with-creds "/path/to/creds.json"))
+```
+
+If you are using it inside Google Cloud Platform services with appropriate service account permissions, 
+you can just provide the project-id using `default-client`:
+
+```clojure
+(def db (db/default-client "project-id"))
+```
+
+## Writing data
+
+We currently provide the methods `add`, `set` and `delete`.
+
+## Queries
+
+We provide the query functions below. 
+
+| firestore-clj | Java API |
+| --- | --- |
+| `filter=` | `.whereEqualTo()` |
+| `filter<` | `.whereLessThan()` |
+| `filter<=` | `.whereLessThanOrEqualTo()` |
+| `filter>` | `.whereGreaterThan()` |
+| `filter>=` | `.whereGreaterThanOrEqualTo()` |
+| `in` | `.whereIn() ` |
+| `contains` | `.whereArrayContains() ` |
+| `contains-any` | `.whereArrayContainsAny() ` |
+| `order-by` | `.orderBy` |
+| `take` | `.limit()` |
+
+You can use `pull` to fetch the results as a map. Here's an example:
+
+```clojure
+(-> (collection db "positions")
+    (filter= "exchange" "bitmex") 
+    (order-by "instrument")
+    (take 2)
+    pull)
+``` 
+
+You can perform multiple equality filters using a map.
+
+```clojure
+(-> (collection db "positions")
+    (filter= {"exchange" "bitmex" 
+              "account" 1}) 
+    pull)
+```
+
+You can use descending order using adding a `:desc`
+
+```clojure
+(-> (collection db "positions")
+    (filter= "account" 1)
+    (order-by "size" :desc) 
+    pull)
+```
+
+If you have the appropriate indexes, you can `order-by` multiple columns:
+
+```clojure
+(-> (collection db "positions")
+    (filter= "account" 1)
+    (order-by "size" :desc "instrument") 
+    pull)
+```
+
+## Real-time data
+
+You can materialize a document/collection reference or query as an `atom` using `->atom`:
+
+```clojure
+(def at @(-> (collection db "positions")
+             (filter= {"exchange" "bitmex" 
+                       "account" 1}) 
+             ->atom)) ; @'ing since ->atom returns a promise
+
+@at
+```
+
+Once you're done, you can just `detach` it.
+
+If you need a lower level utility, you can use `add-listener`. It takes a 2-arity function and merely reifies it
+as an `EventListener`. `snapshot->data` may be useful. Read original docs [here](https://firebase.google.com/docs/firestore/query-data/listen#events-local-changes) 
+for more.
+
+## Contributing and improvements
+
+We welcome [PRs](https://github.com/polvotech/firestore-clj/compare). Here are some things we didn't implement:
+
+* Updates and merges
+* Transactions
+* Batched writes
+* Data pagination and cursors
 
 ## License
 
-Copyright © 2020 F
+Copyright © 2020 Polvo Technologies. 
 
-This program and the accompanying materials are made available under the
-terms of the Eclipse Public License 2.0 which is available at
-http://www.eclipse.org/legal/epl-2.0.
-
-This Source Code may also be made available under the following Secondary
-Licenses when the conditions for such availability set forth in the Eclipse
-Public License, v. 2.0 are satisfied: GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or (at your
-option) any later version, with the GNU Classpath Exception which is available
-at https://www.gnu.org/software/classpath/license.html.
+Distributed under the MIT License
--- src/firestore_clj/core.clj
+++ src/firestore_clj/core.clj
@@ -1,32 +1,33 @@
 (ns firestore-clj.core
   (:require [clojure.java.io :as io]
-            [byte-streams :as bs]
-            [cheshire.core :as cheshire]
-            [environ.core :refer [env]]
-            [polvo.config :as config]
-            [manifold.deferred :as d])
+            [clojure.spec.alpha :as s])
   (:import (com.google.auth.oauth2 GoogleCredentials)
-           (com.google.cloud.firestore Firestore QuerySnapshot CollectionReference EventListener DocumentReference DocumentSnapshot)
+           (com.google.cloud.firestore Firestore QuerySnapshot CollectionReference EventListener DocumentReference DocumentSnapshot Query$Direction)
            (com.google.firebase FirebaseApp FirebaseOptions FirebaseOptions)
-           (com.google.firebase.cloud FirestoreClient)))
+           (com.google.firebase.cloud FirestoreClient)
+           (java.util HashMap)))
 
-(defn- init-firestore-db! ^Firestore []
-  (if (env :local?)
-    (with-open [sa (io/input-stream (env :google-application-credentials))]
-      (let [options (-> (FirebaseOptions/builder)
-                        (.setCredentials (GoogleCredentials/fromStream sa))
-                        (.build))]
-        (FirebaseApp/initializeApp options)
-        (reset! firestore-db (FirestoreClient/getFirestore))))
-
+(defn client-with-creds
+  "Creates a client from a credentials JSON file."
+  ^Firestore [creds-path]
+  (with-open [sa (io/input-stream creds-path)]
     (let [options (-> (FirebaseOptions/builder)
-                      (.setCredentials (GoogleCredentials/getApplicationDefault))
-                      (.setProjectId (env :project-id))
+                      (.setCredentials (GoogleCredentials/fromStream sa))
                       (.build))]
       (FirebaseApp/initializeApp options)
-      (reset! firestore-db (FirestoreClient/getFirestore)))))
+      (FirestoreClient/getFirestore))))
 
-(defn ->data
+(defn default-client
+  "Gets default client i.e. using a service account."
+  ^Firestore [project-id]
+  (let [options (-> (FirebaseOptions/builder)
+                    (.setCredentials (GoogleCredentials/getApplicationDefault))
+                    (.setProjectId project-id)
+                    (.build))]
+    (FirebaseApp/initializeApp options)
+    (FirestoreClient/getFirestore)))
+
+(defn snapshot->data
   "Gets a DocumentReference/CollectionReference/Query's underlying data."
   [s]
   (if (instance? DocumentSnapshot s)
@@ -35,6 +36,14 @@
          (map (fn [d]
                 [(.getId d) (into {} (.getData d))]))
          (into {}))))
+
+(defn pull
+  "Pulls data from a DocumentReference, CollectionReference or Query."
+  [q]
+  (->> q
+       (.get)
+       (.get)
+       snapshot->data))
 
 (defn add-listener
   "Adds a snapshot listener to a DocumentReference/CollectionReference/Query.
@@ -49,7 +58,7 @@
                             (f s e)))))
 
 (defn ->atom
-  "Returns a deferred yielding an atom holding the latest value of a CollectionReference/DocumentReference/Query."
+  "Returns a promise yielding an atom holding the latest value of a CollectionReference/DocumentReference/Query."
   ([q]
    (->atom q identity))
   ([q error-handler]
@@ -57,16 +66,113 @@
          registration (add-listener q (fn [s e]
                                         (if (some? e)
                                           (error-handler e)
-                                          (reset! a (->data s)))))
-         d            (d/deferred)]
+                                          (reset! a (snapshot->data s)))))
+         d            (promise)]
      (alter-meta! a assoc :registration registration)
      (add-watch a :waiting-first-val (fn [_ _ _ _]
                                        (do
                                          (remove-watch a :waiting-first-val)
-                                         (d/success! d a))))
+                                         (deliver d a))))
      d)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn detach
+  "Detaches an atom built with ->atom."
+  [a]
+  (.remove (:registration (meta a))))
+
+(defn collection
+  "Returns a CollectionReference for the collection of given name."
+  ^CollectionReference [^Firestore db ^String coll-name]
+  (.collection db coll-name))
+
+(defn document
+  "Gets a document from a collection reference."
+  ^DocumentReference [^CollectionReference c ^String id]
+  (.document c id))
+
+(defn add
+  "Adds a document to a collection. Its id will be automatically generated. This is a blocking operation."
+  [^CollectionReference c m]
+  (-> c (.add (HashMap. m)) (.get)))
+
+(defn set
+  "Creates or overwrites a document. This is a blocking operation."
+  [^CollectionReference c doc-name m]
+  (-> c (.document doc-name) (.set (HashMap. m)) (.get)))
+
+(defn delete
+  "Deletes a document."
+  [^DocumentReference d]
+  (-> (.delete d) (.get)))
+
+(defn filter=
+  "Filters where field = value. A map may be used for checking multiple equalities."
+  ([q m]
+   (reduce (fn [q' [field value]]
+             (filter= q' field value))
+     q
+     m))
+  ([q field value]
+   (.whereEqualTo q field value)))
+
+(defn filter<
+  "Filters where field < value."
+  [q field value]
+  (.whereLessThan q field value))
+
+(defn filter<=
+  "Filters where field <= value."
+  [q field value]
+  (.whereLessThanOrEqualTo q field value))
+
+(defn filter>
+  "Filters where field > value."
+  [q field value]
+  (.whereGreaterThan q field value))
+
+(defn filter>=
+  "Filters where field >= value."
+  [q field value]
+  (.whereGreaterThanOrEqualTo q field value))
+
+(defn take
+  "Limits results to a certain number."
+  [q n]
+  (.limit q n))
+
+(defn in
+  "Filters where field is one of the values in arr."
+  [q field arr]
+  (.whereIn q field arr))
+
+(defn contains
+  "Filters where field contains value."
+  [q field value]
+  (.whereArrayContains q field value))
+
+(defn contains-any
+  "Filters where field contains one of the values in arr."
+  [q field arr]
+  (.whereArrayContainsAny q field arr))
+
+(s/def ::direction #{:asc :desc})
+(s/def ::ordering (s/+ (s/alt
+                         :field-direction (s/cat :field string? :direction ::direction)
+                         :field string?)))
+
+(defn order-by
+  "Orders by a sequence of fields with optional directions. Notice that ordering by multiple fields
+  requires creation of a composite index."
+  [q & ordering]
+  (let [conformed (s/conform ::ordering ordering)]
+    (if (= conformed ::s/invalid)
+      (throw (ex-info "Invalid ordering: " (s/explain-str ::ordering ordering)))
+      (reduce (fn [q' [spec value]]
+                (case spec
+                  :field (.orderBy q' value)
+                  :field-direction (let [{:keys [field direction]} value]
+                                     (if (= direction :desc)
+                                       (.orderBy q' field Query$Direction/DESCENDING)
+                                       (.orderBy q' field)))))
+              q
+              conformed))))
