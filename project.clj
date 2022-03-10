4e2bf66a697b51e6235cd13dab8de9d0799c8f63
Yannick <yannick.lehnhausen@janitza.de>
2022-03-10 14:22:05 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject lurodrigo/firestore-clj "1.2.1"
+(defproject lurodrigo/firestore-clj "1.2.2"
   :description "A Firestore API for Clojure. Provides tools for doing single pulls and writes, streaming real-time data, batched writes and transactions."
   :url "https://github.com/polvotech/firestore-clj"
   :license {:name "MIT LICENSE"
-            :url  "https://github.com/lurodrigo/firestore-clj/blob/master/LICENSE"}
+            :url "https://github.com/lurodrigo/firestore-clj/blob/master/LICENSE"}
   :scm {:name "git" :url "https://github.com/lurodrigo/firestore-clj"}
   :plugins [[lein-codox "0.10.7"]]
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/core.match "1.0.0"]
-                 [com.google.firebase/firebase-admin "6.12.2"]
+                 [com.google.firebase/firebase-admin "8.1.0"]
                  [manifold "0.1.8"]]
   :aot [firestore-clj.google.fake-credentials
         firestore-clj.google.emulator-channel-configurator]
   :profiles {:dev {:dependencies [[com.taoensso/timbre "4.10.0"]
                                   [com.fzakaria/slf4j-timbre "0.3.19"]
-                                  [com.google.cloud/google-cloud-logging-logback "0.116.0-alpha"]]
+                                  [com.google.cloud/google-cloud-logging-logback "0.123.5-alpha"]]
                    :source-paths ["test/clj"]
                    :java-source-paths ["test/java"]}}
   :global-vars {*warn-on-reflection* true}
